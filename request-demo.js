70ac2e2ce4a502b14823f2619841c790b3cb677d
miguelcruz-am <163028971+miguelcruz-am@users.noreply.github.com>
2024-05-23 11:02:57 +0100

    Update request-demo.js

--- request-demo.js
+++ request-demo.js
@@ -1,4 +1,5 @@
 const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
 document.addEventListener("DOMContentLoaded", () => {
   (function (windowRef, documentRef, fileLocation, functionName) {
     windowRef["LeanDataCalendaringObjName"] = functionName;
@@ -8,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const scriptElement = documentRef.createElement("script");
     const targetScript = document.getElementsByTagName("script")[0];
     const targetHead = document.getElementsByTagName("head")[0];
-    scriptElement.async = 1;
+    scriptElement.async = true;
     scriptElement.src = fileLocation;
     if (targetScript) {
       targetScript.parentNode.insertBefore(scriptElement, targetScript);
@@ -21,6 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
     "https://app.leandata.com/js-snippet/ld_calendaring.js",
     "LDBookIt"
   );
+
   // TODO: replace values below with your SFDC id and BookIt node name
   LDBookIt("00D5I000000Fr4cUAC", "New Inbound Lead", false, {
     apiRoutingOn: true,
@@ -31,14 +33,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
   (function () {
     let isSubmitting = false;
-    // Attaching BookIt process to your form submit button
+
     const attachToForm = () => {
       let registered = false;
       console.log("Attempting to attach to form");
       const myForm = document.querySelector("#email-form");
-      if (myForm != null) {
+      if (myForm) {
         registered = true;
-        // TODO: Verify selector below
         myForm
           .querySelector('input[type="submit"]')
           .addEventListener("click", async (event) => {
@@ -52,7 +53,6 @@ document.addEventListener("DOMContentLoaded", () => {
                 return;
               }
 
-              // make an array of invalid domains
               const invalidDomains = [
                 "gmail.com",
                 "yahoo.com",
@@ -61,30 +61,20 @@ document.addEventListener("DOMContentLoaded", () => {
                 "msn.com",
               ];
 
-              // validate email field
-              // get the email field
               const email = $(".business-only-email-field");
-              // split email at '@' character to get domain
-              let domainPart = email.val().split("@")[1];
-              // convert the domain to lowercase before comparing, just in case the user typed it in caps
-              domainPart = domainPart.toLowerCase();
-              // if the domain exists in the invalidDomains array
-              if (invalidDomains.indexOf(domainPart) !== -1) {
-                // add the 'no-pea-message' class to show the error message
-                email.addClass("has-error");
-                // focus the email field
-                email.focus();
-                email.on("input", () => {
-                  // remove error message when user restarts typing
-                  email.removeClass("has-error");
-                  $(".no-pea-message").hide();
-                });
-                $(".no-pea-message").show();
+              let domainPart = email.val().split("@")[1].toLowerCase();
 
-                // prevent form submission
+              if (invalidDomains.includes(domainPart)) {
+                email
+                  .addClass("has-error")
+                  .focus()
+                  .on("input", () => {
+                    email.removeClass("has-error");
+                    $(".no-pea-message").hide();
+                  });
+                $(".no-pea-message").show();
                 return false;
               } else {
-                // else if email is not invalid
                 const loadingScreen = document.querySelector(".loading-screen");
                 const emailErrorMsg = document.querySelector(
                   ".email-form-error-message"
@@ -93,106 +83,73 @@ document.addEventListener("DOMContentLoaded", () => {
                 loadingScreen.style.opacity = 1;
                 emailErrorMsg.style.display = "none";
                 email.removeClass("has-error");
-                setTimeout(
-                  () =>
-                    (loadingScreen.querySelector(
-                      ".loading-text"
-                    ).style.opacity = 0.5),
-                  2000
-                );
 
-                // remove the 'no-pea-message' class and hide the error message
+                setTimeout(() => {
+                  loadingScreen.querySelector(
+                    ".loading-text"
+                  ).style.opacity = 0.5;
+                }, 2000);
+
                 $(".no-pea-message").hide();
 
-                let data = null;
-                let emailStatus = null;
-
                 try {
-                  // make a POST fetch request to validate the email
-                  const response = await fetch(
-                    "https://app.amplemarket.com/api/v1/amplemarket_inbounds/validate_email",
-                    {
-                      method: "POST",
-                      headers: {
-                        "Content-Type": "application/json",
-                      },
-                      body: JSON.stringify({ email: email.val() }),
-                    }
-                  );
-                  // get the JSON response
-                  data = await response.json();
-                  emailStatus = data?.status;
+                  const [validateResponse, enrichResponse] = await Promise.all([
+                    fetch(
+                      "https://app.amplemarket.com/api/v1/amplemarket_inbounds/validate_email",
+                      {
+                        method: "POST",
+                        headers: {
+                          "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({ email: email.val() }),
+                      }
+                    ),
+                    fetch(
+                      `https://app.amplemarket.com/api/v1/amplemarket_inbounds/enrich_person?email=${encodeURIComponent(
+                        email.val()
+                      )}`
+                    ),
+                  ]);
 
-                  // if the request returns a 400 status or a "valid"=false response, don't proceed any further
-                  if (response.status === 400 || data?.valid === false) {
-                    loadingScreen.style.display = "none";
-                    // set the error message
-                    emailErrorMsg.style.display = "block";
-                    email.addClass("has-error");
-                    // focus the email field
-                    email.focus();
-                    email.on("input", () => {
-                      // remove error message when user restarts typing
-                      email.removeClass("has-error");
-                      emailErrorMsg.style.display = "none";
-                    });
-                    return false;
+                  const validateData = await validateResponse.json();
+                  if (!validateResponse.ok || validateData?.valid === false) {
+                    throw new Error("Email validation failed");
                   }
+
+                  const enrichData = await enrichResponse.json();
+
+                  let partnerKey = growsumo.data.partner_key;
+
+                  enrichedData = {
+                    title: enrichData.title || null,
+                    person_location: enrichData.location || null,
+                    size: enrichData.company?.size || null,
+                    sales_team_size_enriched:
+                      enrichData.company?.department_headcount?.sales || null,
+                    industry: enrichData.company?.industry || null,
+                    company_location: enrichData.company?.location || null,
+                    is_b2b: enrichData.company?.is_b2b || null,
+                    is_b2c: enrichData.company?.is_b2c || null,
+                    partner_key: partnerKey,
+                    email_status: validateData.status || null,
+                  };
                 } catch (error) {
-                  console.log(error);
+                  console.error("Validation or Enrichment Error:", error);
+                  if (error.message === "Email validation failed") {
+                    loadingScreen.style.display = "none";
+                    emailErrorMsg.style.display = "block";
+                    email
+                      .addClass("has-error")
+                      .focus()
+                      .on("input", () => {
+                        email.removeClass("has-error");
+                        emailErrorMsg.style.display = "none";
+                      });
+                    return false; // Block submission if email validation fails
+                  }
+                  // Log enrichment errors but do not block submission
                 }
 
-                let enrichRes;
-                try {
-                  const enrichReq = await fetch(
-                    `https://app.amplemarket.com/api/v1/amplemarket_inbounds/enrich_person?email=${encodeURIComponent(
-                      email.val()
-                    )}`
-                  );
-                  if (!enrichReq.ok) throw new Error("Failed to fetch");
-                  enrichRes = await enrichReq.json();
-                } catch (error) {
-                  console.error("Error fetching enriched data:", error);
-                  enrichRes = null;
-                }
-
-                let partnerKey = growsumo.data.partner_key;
-
-                enrichedData = {
-                  title: enrichRes ? enrichRes.title : null,
-                  person_location: enrichRes ? enrichRes.location : null,
-                  size:
-                    enrichRes && enrichRes.company
-                      ? enrichRes.company.size
-                      : null,
-                  sales_team_size_enriched:
-                    enrichRes &&
-                    enrichRes.company &&
-                    enrichRes.company.department_headcount
-                      ? enrichRes.company.department_headcount.sales
-                      : null,
-                  industry:
-                    enrichRes && enrichRes.company
-                      ? enrichRes.company.industry
-                      : null,
-                  company_location:
-                    enrichRes && enrichRes.company
-                      ? enrichRes.company.location
-                      : null,
-                  is_b2b:
-                    enrichRes && enrichRes.company
-                      ? enrichRes.company.is_b2b
-                      : null,
-                  is_b2c:
-                    enrichRes && enrichRes.company
-                      ? enrichRes.company.is_b2c
-                      : null,
-                  partner_key: partnerKey,
-                  email_status: emailStatus,
-                  // technologies: enrichRes && enrichRes.company ? enrichRes.company.technologies.join(', ') : null,
-                };
-
-                // run bookit code
                 BookItAPIRouting(myForm);
               }
             } else {
@@ -201,30 +158,26 @@ document.addEventListener("DOMContentLoaded", () => {
           });
       }
       if (!registered) {
-        setTimeout(() => {
-          attachToForm();
-        }, 1000);
+        setTimeout(attachToForm, 1000); // Necessary if form might load later
       }
     };
+
     const BookItAPIRouting = (myForm) => {
       let options = {
         MAType: "Custom",
         formHandle: myForm,
         onError: (error) => {
-          // TODO: Add Zap flow trigger here
           postFullRequestData();
         },
         afterSubmit: async (formData) => {
           growsumo.data.name = formData["Name"];
           growsumo.data.email = formData["Company-Email"];
-          let domainPart = formData["Company-Email"].split("@")[1];
-          domainPart = domainPart.toLowerCase();
+          let domainPart = formData["Company-Email"]
+            .split("@")[1]
+            .toLowerCase();
           growsumo.data.customer_key = domainPart;
           await growsumo.createSignup();
-
-          setTimeout(() => {
-            window.location.href = "/thanks-demo";
-          }, 150);
+          setTimeout(() => (window.location.href = "/thanks-demo"), 10);
         },
         afterRouting: (formData, responseData) => {
           window.localStorage.setItem(
@@ -241,21 +194,17 @@ document.addEventListener("DOMContentLoaded", () => {
           );
         },
         customValidation: () => {
-          // TODO: Add form validation to stop LD from routing invalid data. Sample validation provided below.
           const requiredFields = myForm.querySelectorAll("[required]");
-          console.log(requiredFields);
           let isValid = true;
           requiredFields.forEach((field) => {
-            if (field.value === "") {
+            if (!field.value) {
               isValid = false;
             }
           });
           return isValid;
         },
         formDataCollector: (hiddenFieldNames) => {
-          let formData, formHiddenFieldNames;
-          [formData, formHiddenFieldNames] = CollectFormData(myForm);
-          //splits Name field into first and last by first space
+          let [formData, formHiddenFieldNames] = CollectFormData(myForm);
           let name = formData.Name.trim();
           if (name.includes(" ")) {
             const [firstName, ...lastName] = name.split(" ");
@@ -265,12 +214,9 @@ document.addEventListener("DOMContentLoaded", () => {
             formData.firstname = name;
             formData.lastname = "";
           }
-
-          // Merge enrichedData with formData
           if (enrichedData) {
             formData = { ...formData, ...enrichedData };
           }
-
           return formData;
         },
         hiddenFieldSetter: (
@@ -279,25 +225,17 @@ document.addEventListener("DOMContentLoaded", () => {
           responseData,
           hiddenFieldNames
         ) => {
-          // TODO: Use this function to set the log id field on your form so it gets passed to your MA.
           const logIdField = myForm.querySelector(
             'input[name="ld_bookit_log_id"]'
           );
           logIdField.value = responseData.formInput.ld_bookit_log_id;
         },
         beforeSubmit: (formData, responseData) => {
-          // TODO: Add Zap flow trigger here
           postFullRequestData();
         },
-        /* afterSubmit: (formData, responseData) => {
-            if (responseData.calendarLink) {
-              LDBookIt.redirect(options);
-            }
-          }, */
         formSubmitter: () => {
           isSubmitting = true;
           console.log("is submitting");
-          // TODO: Verify selector below
           myForm.querySelector('input[type="submit"]').click();
         },
       };
@@ -306,11 +244,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     attachToForm();
   })();
+
   const CollectFormData = (myForm) => {
     let formData = {};
     let hiddenFieldNames = new Set();
     const elements = myForm.elements;
-    // get all form data - this errs on getting too many fields
     for (let i = 0; i < elements.length; i++) {
       let elem = elements[i];
       let apiName = elem.name;
@@ -321,13 +259,8 @@ document.addEventListener("DOMContentLoaded", () => {
         case "email":
           formData[apiName] = elem.value;
           break;
-          O;
         case "select-one":
-          const selectedIndex = elem.selectedIndex;
-          apiName = elem.name;
-          formData[apiName] = selectedIndex
-            ? elem.options[elem.selectedIndex].value
-            : "";
+          formData[apiName] = elem.options[elem.selectedIndex].value;
           break;
         case "checkbox":
           formData[apiName] = elem.checked;
@@ -343,9 +276,6 @@ document.addEventListener("DOMContentLoaded", () => {
           break;
       }
     }
-    // TODO: ensure you are getting all the data you need from the form
-    console.log("formData", formData);
-    console.log("hiddenFieldNames", hiddenFieldNames);
     return [formData, hiddenFieldNames];
   };
 });
