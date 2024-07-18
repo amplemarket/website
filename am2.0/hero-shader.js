77e2057707dd46a31b0931a2699da7cb2905d6db
miguelcruz-am <163028971+miguelcruz-am@users.noreply.github.com>
2024-07-18 13:42:58 +0100

    Update hero-shader.js

--- am2.0/hero-shader.js
+++ am2.0/hero-shader.js
@@ -9,75 +9,161 @@ const vertexShaderSource = `
 `;
 
 const fragmentShaderSource = `
+    #ifdef GL_ES
     precision highp float;
+    #endif
+
+    #define PI 3.14159265359
+    #define TWO_PI 6.28318530718
 
     uniform vec2 u_resolution;
     uniform float u_time;
+    uniform vec2 u_mouse;
     uniform float u_dpr;
     uniform vec3 u_col1;
     uniform vec3 u_col2;
     uniform vec3 u_col3;
     uniform vec3 u_col4;
 
-    float rand(vec2 co) {
-        return fract(sin(dot(co.xy, vec2(12.9898, 78.233))) * 43758.5453) / u_dpr;
+    float rand(vec2 co){
+        return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453) / u_dpr;
     }
 
-    vec4 circle(vec2 st, vec2 center, float radius, float blur, vec3 col) {
-        float dist = distance(st, center) * 2.0;
-        float alpha = 1.0 - smoothstep(radius, radius + blur, dist);
-        vec4 color = vec4(col, 1.0) * alpha;
-        return color;
+    float map(float value, float min1, float max1, float min2, float max2) {
+        return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
     }
 
-    void main() {
-        vec2 st = gl_FragCoord.xy / u_resolution.xy;
-        float aspect = u_resolution.x / u_resolution.y;
-        vec2 adjustedSt;
-        if(u_resolution.x >= u_resolution.y) {
-            adjustedSt = (st - 0.5) * vec2(aspect, 1.0) + 0.5;
-        } else {
-            adjustedSt = (st - 0.5) * vec2(1.0, 1.0/aspect) + 0.5;
-        }
+    vec4 circle(vec2 st, vec2 center, float radius, float blur, vec3 col){
+        float dist = distance(st,center)*2.0;
+        vec4 f_col = vec4(1.0-smoothstep(radius, radius + blur, dist));
+        f_col.r *= col.r;
+        f_col.g *= col.g;
+        f_col.b *= col.b;
+        return f_col;
+    }
 
-        vec3 col1 = u_col1; // blue
-        vec3 col2 = u_col2; // purple
-        vec3 col3 = u_col3; // orange
-        vec3 col4 = u_col4; // mint
-
-        vec4 color = vec4(0.0);
-
-        vec2 blueCenter = vec2(0.5 + cos(u_time * 1.4) * sin(u_time * 1.2) * 0.1, 0.5 + sin(u_time * 1.2) * cos(u_time * 1.5) * 0.1);
-        vec2 purpleCenter = vec2(0.5 + cos(u_time * 0.1) * sin(u_time * 2.6) * 0.1, 0.5 + sin(u_time * 3.4) * cos(u_time * 0.1) * 0.1);
-        vec2 orangeCenter = vec2(0.5 + cos(u_time * 2.6) * sin(u_time * 0.4) * 0.1, 0.5 + sin(u_time * 1.1) * cos(u_time * 2.7) * 0.1);
-        vec2 mintCenter = vec2(0.5 + cos(u_time * 1.) * sin(u_time * 3.4) * 0.1, 0.5 + sin(u_time * .1) * cos(u_time * 1.7) * 0.1);
-
-        adjustedSt.x += sin(u_time * 0.5 + adjustedSt.x * 4.0) * 0.07 * cos(u_time * 0.6 + adjustedSt.y * 8.0) * 0.8;
-        adjustedSt.y += cos(u_time * 0.7 + adjustedSt.x * 2.0) * 0.25 * sin(u_time * 0.4 + adjustedSt.y * 10.0) * 0.2;
-
-        color += circle(adjustedSt, blueCenter, 0., 0.75, col4);
-            - color * circle(adjustedSt, blueCenter, 0., 0.75, vec3(1.));
-    
-        color += circle(adjustedSt, purpleCenter, 0., 0.75, col3)
-            - color * circle(adjustedSt, purpleCenter, 0., 0.75, vec3(1.));
-    
-        color += circle(adjustedSt, orangeCenter, 0., 0.75, col2)
-            - color * circle(adjustedSt, orangeCenter, 0., 0.75, vec3(1.));
+    void main(){
+        // UNCHANCHED ST
+        vec2 fst = gl_FragCoord.xy/u_resolution.xy;
         
-        color += circle(adjustedSt, mintCenter, 0., 0.5, col1)
-            - color * circle(adjustedSt, mintCenter, 0., 0.5, vec3(1.));
+        // ASPECT RATIO
+        float aspect = u_resolution.x/u_resolution.y;
 
-        float noise = rand(st * 10.0) * 0.2;
-        color.rgb *= 1.0 - noise;
+        // PURPLE & GREEN BLOB ST
+        vec2 pst = fst * vec2(aspect, 1.);
 
-        gl_FragColor = color * 0.5;
+        // MINT BLOB ST
+        vec2 mst = fst;
+
+        // MOUSE
+        vec2 m = u_mouse.xy/u_resolution.xy;
+        //m *= u_dpr;
+        //m.x *= aspect;
+
+        // HEADER COLORS
+        //vec3 col1 = vec3(159., 215., 236.) / 255.;
+        vec3 col1 = vec3(172., 233., 255.) / 255.;
+        vec3 col2 = vec3(213., 160., 255.) / 255.;
+        vec3 col3 = vec3(249., 139., 140.) / 255.;
+
+        // INIT MAIN COLOR
+        vec4 color = vec4(0.);
+
+        // PURPLE BLOB VARIABLES
+        vec2 purpleC = vec2(m.x, 1.-m.y);
+        float purpleR = .3;
+        float purpleB = .5;
+        vec3 purpleCol = col1;
+
+        // MINT BLOB VARIABLES
+        vec2 mintC = vec2(.5+sin(u_time*.4)*.5*cos(u_time*.2)*.5, .5+sin(u_time*.3)*.5*cos(u_time*.5)*.5);
+        float mintR = .5;
+        float mintB = .75;
+        vec3 mintCol = col2;
+
+        // GREEN BLOB VARIABLES
+        vec2 greenC = vec2((.5+cos(u_time*.5)*.5*sin(u_time*.2)*.5)*aspect, .5+cos(u_time*.4)*(.5)*sin(u_time*.3)*.5);
+        float greenR = .5;
+        float greenB = .75;
+        vec3 greenCol = col3;
+
+        // DISTORT PURPLE & GREEN BLOBS
+        pst.x += sin(u_time*.15+pst.x*19.)*.37 * cos(u_time*.46+pst.y*25.)*.28;
+        pst.y += cos(u_time*.27+pst.x*4.)*.45 * sin(u_time*.24+pst.y*8.)*.22;
+
+        // DISTORT MINT BLOB
+        mst.x += cos(u_time*.37+mst.x*15.)*.21 * sin(u_time*.14+mst.y*7.)*.29 * (m.x - .5 * u_dpr) * 5.;
+        mst.y += sin(u_time*.15+mst.x*13.)*.37 * cos(u_time*.36+mst.y*5.)*.12 * (m.y - .5 * u_dpr) * 5.;
+
+        vec4 color1 = vec4(0.); // BLOB FOR PURPLE
+        vec4 color2 = vec4(0.); // BLOB FOR GREEN
+        vec4 color3 = vec4(0.); // PURPLE BLOB
+        vec4 color4 = vec4(0.); // GREEN BLOB
+        vec4 color5 = vec4(0.); // GREEN BLOB
+        vec4 color6 = vec4(0.);
+
+        // add WHITE BLOB for PURPLE BLOB
+        color1 += vec4(
+            (circle(mst, mintC, mintR, mintB, vec3(1.))
+            // remove mint blob from purple blob
+            - circle(mst, mintC, mintR, mintB, vec3(1.)) * circle(mst, greenC, greenR, greenB, vec3(1.))
+            )
+        );
+
+        // add WHITE BLOB for GREEN BLOB
+        color2 += vec4(
+            // RGB
+            (circle(mst, mintC, mintR, mintB, vec3(1.))
+            // remove mint blob from green blob
+            - circle(mst, mintC, mintR, mintB, vec3(1.)) * circle(mst, purpleC, purpleR, purpleB, vec3(1.))
+            )
+        );
+
+        // remove white information from purple and green blobs intersection
+        color1 -= color1 * color2;
+        color2 -= color1 * color2;
+
+        // give them the purple and green colors
+        color3 = color1;
+        color4 = color2;
+
+        color3.rgb *= purpleCol;
+        color4.rgb *= greenCol;
+
+        // add them to the main color
+        color += color3;
+        color += color4;
+
+        color5 += vec4(
+            // RGB
+            (circle(mst, greenC, greenR, greenB, vec3(1.))
+            // remove mint blob from green blob
+            - circle(mst, greenC, greenR, greenB, vec3(1.)) * circle(mst, mintC, mintR, mintB, vec3(1.))
+            )
+        );
+
+        color5 -= color1 * color2;
+
+        color5.rgb *= mintCol;
+
+        color += color5;
+
+        // add MINT BLOB
+        color += circle(mst, mintC, mintR, mintB, mintCol)
+            * (color1 - circle(mst, mintC, mintR, mintB, vec3(1.)))
+            * (color2 - circle(mst, mintC, mintR, mintB, vec3(1.)));
+
+        float noise = rand(fst*10.) * .2;
+
+        color.rgb *= 1. - vec3(noise);
+
+        gl_FragColor = color;
     }
 `;
 
-console.log("cenas");
-
 const canvas = document.querySelector(".shader-canvas");
 const gl = canvas.getContext("webgl");
+const m = { x: 0, y: 0 };
 
 if (!gl) {
   alert("WebGL not supported, falling back on experimental-webgl");
@@ -184,10 +270,13 @@ function render() {
     offset
   );
 
+  m.x += (mouse[0] - m.x) * 0.05;
+  m.y += (mouse[1] - m.y) * 0.05;
+
   const resolution = [gl.canvas.width, gl.canvas.height];
   gl.uniform2fv(u_resolution, resolution);
   gl.uniform1f(u_time, (Date.now() - startTime) * 0.001);
-  gl.uniform2fv(u_mouse, mouse);
+  gl.uniform2fv(u_mouse, [m.x, m.y]);
   gl.uniform1f(u_dpr, window.devicePixelRatio);
   gl.uniform3fv(u_col1, [45 / 255, 114 / 255, 240 / 255]); // blue
   gl.uniform3fv(u_col2, [168 / 255, 139 / 255, 250 / 255]); // purple
